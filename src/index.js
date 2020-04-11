0a3f7af6952b32a4a9a101bff9f10a9b3b13f708
DevSp <contatodevsp@gmail.com>
2020-04-11 19:04:24 -0300

    Configurando as rotas

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+// configurando as variaveis de ambiente
 require('dotenv').config()
 const express = require('express')
 const app = express()
 
 
-app.get('/', (req, res) => res.send('Testando...'))
+// configurando as rotas
+app.use('/user', require('./routes/userRouter'))
 
+
+// servindo a aplicação 
 app.listen(process.env.PORT, () => console.log('Servidor rodando na porta: ', process.env.PORT))
