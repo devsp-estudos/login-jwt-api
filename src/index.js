13778eebbaadd0b774d65ae5351c25393e681e20
DevSp <contatodevsp@gmail.com>
2020-04-11 19:31:41 -0300

    Instalando mongoose, conectando o db

--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ const express = require('express')
 const app = express()
 
 
+// conectando ao banco 
+const mongoose = require('mongoose')
+const options = { useNewUrlParser: true, useUnifiedTopology: true }
+mongoose.connect(process.env.MONGO_URL, options, (error) => {
+    if (error) console.log('Error: ', error)
+    console.log('> Banco conectado :)')
+})
+
+
 // configurando as rotas
 app.use('/user', require('./routes/userRouter'))
 
 
 // servindo a aplicação 
-app.listen(process.env.PORT, () => console.log('Servidor rodando na porta: ', process.env.PORT))
+app.listen(process.env.PORT, () => console.log('> Servidor rodando na porta: ', process.env.PORT))
